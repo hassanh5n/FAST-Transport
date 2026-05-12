f03bf368e23cd9498404e30f5dcd603f51b8d875
Shaikh Hassan Nafees <149721057+hassanh5n@users.noreply.github.com>
2026-05-12 19:31:15 +0500

    fixed ports exposure

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -12,7 +12,8 @@ CORS_ALLOWED_ORIGINS = [os.environ['FRONTEND_URL']]
 # Nginx terminates SSL, so Django sees HTTP internally.
 # These settings tell Django the connection is actually HTTPS.
 SECURE_PROXY_SSL_HEADER      = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT           = True
+#SECURE_SSL_REDIRECT           = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE         = True
 CSRF_COOKIE_SECURE            = True
 SECURE_HSTS_SECONDS           = 31536000   # 1 year
